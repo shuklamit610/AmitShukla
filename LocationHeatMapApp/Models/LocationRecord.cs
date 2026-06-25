6b8e4145d91e79a60f2f74804258b776dc4bcc96
shuklamit610 <shuklamit610@gmail.com>
2026-06-25 13:27:29 -0400

    Add files via upload

---
+++ LocationHeatMapApp/Models/LocationRecord.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+
+namespace LocationHeatMapApp.Models;
+
+/// <summary>
+/// Represents a saved user location.
+/// </summary>
+/// 
+public class LocationRecord
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+
+    public DateTime Timestamp { get; set; }
+}
